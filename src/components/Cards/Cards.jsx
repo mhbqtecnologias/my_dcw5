fd0e36aec63b0b2b98025d9286d14c1677de8ed2
Marcio de Holanda Barbosa Quaresma <mhbqtecnologias@gmail.com>
2022-09-02 16:45:01 -0300

    Novidades

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -26,7 +26,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate }}) => {
                     </Grid>
                     <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                         <CardContent>
-                            <Typography variant = "h5" color = "textPrimary" gutterBottom>Recuperados</Typography>
+                            <Typography variant = "h5" color = "textPrimary" gutterBottom>SALVOS</Typography>
                             <Typography variant = "h5">
                                 <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
                             </Typography>
