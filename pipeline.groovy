07598a75b2e2b95b612dcf6a08a4b06ad752117f
Marcio de Holanda Barbosa Quaresma <mhbqtecnologias@gmail.com>
2022-09-02 22:35:08 -0300

    Mais nova alteraciones

--- pipeline.groovy
+++ pipeline.groovy
@@ -36,3 +36,44 @@ pipeline {
 		}
     }
 }
+stage('Send image to Docker Hub') {
+            steps{
+                script {
+                    docker.withRegistry( '', registryCredential) {
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+    	stage('Deploy') {
+		    steps{
+                step([$class: 'AWSCodeDeployPublisher',
+                    applicationName: 'myappcodeploy',
+                    awsAccessKey: "AKIATTYCTNZOH23WO4NN", // ALTERAR
+                    awsSecretKey: "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", // ALTERAR
+                    credentials: 'awsAccessKey',
+                    deploymentGroupAppspec: false,
+                    deploymentGroupName: 'groupcodeployapp', // ALTERAR
+                    deploymentMethod: 'deploy',
+                    excludes: '',
+                    iamRoleArn: '',
+                    includes: '**',
+                    pollingFreqSec: 15,
+                    pollingTimeoutSec: 600,
+                    proxyHost: '',
+                    proxyPort: 0,
+                    region: 'us-east-1', // CHECAR
+                    s3bucket: 'dcw5-s3bucket', // ALTERAR
+                    s3prefix: '', 
+                    subdirectory: '',
+                    versionFileName: '',
+                    waitForCompletion: true])
+            }
+        }
+    	stage('Cleaning up') {
+        	steps {
+            	sh "docker rmi $registry:develop"
+        	}
+		}
+    }
+}
